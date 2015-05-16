571a6293146fe8706dc0004f6137943dee4af09c
mtoshi <mtoshi.g@gmail.com>
2015-05-16 22:00:06 +0900

    Ver 0.1.8

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ EXCLUDE_FROM_PACKAGES = []
 
 setup(
     name="rancidcmd",
-    version="0.1.7",
+    version="0.1.8",
     description='RANCID Command Wrapper.',
     long_description=README,
     author='Toshikatsu Murakoshi',
